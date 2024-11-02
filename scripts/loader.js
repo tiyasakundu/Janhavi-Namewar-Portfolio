911e8353ffcdc6a75f3d8bb6666946151fa20bc8
tiyasakundu <tiyasakundu20@gmail.com>
2024-11-02 21:09:18 +0530

    minified

--- scripts/loader.js
+++
@@ -1,9 +0,0 @@
-// This script will control the transition from loader.html to the actual page
-
-// Redirect to the content page once the page is loaded
-window.addEventListener('load', function() {
-    // Wait for a short period to show the loader, then redirect to the main page
-    setTimeout(() => {
-        window.location.href = 'index.html'; // Redirect to your actual content page (e.g., index.html)
-    }, 2000); // Adjust delay to fit your needs
-});
